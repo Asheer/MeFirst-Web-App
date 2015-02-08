f0b8a727c9ca31f248c15f7900849afae4f3dc32
System Administrator <root@Finals-Mac.local>
2015-02-08 08:38:49 -0800

    LOL

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def scrape(username, password):
     app.logger.debug(classAndDes)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True, port=port)
+    app.run(port=port)
